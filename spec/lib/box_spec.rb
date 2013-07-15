76312ca253f6f512c8f8d4f33183630e5e8ebcea
ignaciocontreras <ignacio@ignacio.cat>
2013-07-15 22:44:53 +0200

    Fix box_spec

--- spec/lib/box_spec.rb
+++ spec/lib/box_spec.rb
@@ -3,24 +3,20 @@ require 'pathname'
 
 
 describe Caja::Box do
+  let(:bad_path) { "~/i-dont-exist.db"}
+  let(:good_path) { "./caja.db" }
   
-  context "initialization" do
-    
-    it "should raise an error if the data store can not be created" do
-      let(:bad_path) { "~/i-dont-exist.db"}
-      expect { Caja::Box.new(bad_path) }.to raise_error(Caja::IOError)
-      PathName::File.exist?(bad_path).should_not be_true
-    end
-
-    it "should create a new file in case the initialization is successful" do
-      let(:good_path) { "./caja.db" }
-      before { fail if PathName::File.exist?(good_path) }
-      after { PathName::File.delete(good_path) }
-
-      box = Caja::Box.new(good_path)
-      PathName::File.exist?(good_path).should be_true
-    end
-  
+  it "should raise an error if the data store can not be created" do
+    expect { Caja::Box.new(bad_path) }.to raise_error(Caja::IOError)
+    PathName::File.exist?(bad_path).should_not be_true
   end
 
+  it "should create a new file in case the initialization is successful" do
+    before { fail if PathName::File.exist?(good_path) }
+    after { PathName::File.delete(good_path) }
+
+    box = Caja::Box.new(good_path)
+    PathName::File.exist?(good_path).should be_true
+  end
+  
 end
