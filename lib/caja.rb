63dcbaa757458e4b1f3b0d0a74d39448c49e1b13
ignaciocontreras <ignacio@ignacio.cat>
2013-07-19 22:37:25 +0200

    Update lib and spec, fix syntax errors.

--- lib/caja.rb
+++ lib/caja.rb
@@ -1,9 +1,45 @@
 require "caja/version"
+require "caja_store"
+require 'file'
 
 module Caja
 
-  class IOError < StandardError; end
+  def valid_new_file?(path)
+    dirname = File.dirname(path)
+    if File.exist?(path)
+      return false
+    else
+      return File.exist?(dirname)
+    end
+  end
 
-  class Store; end
+  class Box
+    attr_reader :path
+
+    def initialize(path)
+      unless valid_new_file(path) do
+        raise IOError, "invalid path"
+      end
+      @store = Caja::Store.new(path)
+      @path = path
+    end
+
+    def store(key, value)
+      @store.store(key, value)
+    end
+
+    def append(key, value)
+      @store.append(key, value)
+    end
+
+    def fetch(key)
+      @store.fetch(key)
+    end
+
+    def delete(key)
+      @store.delete(key)
+    end
+
+  end
 
 end
--- spec/lib/box_spec.rb
+++ spec/lib/box_spec.rb
@@ -3,20 +3,21 @@ require 'pathname'
 
 
 describe Caja::Box do
-  let(:bad_path) { "~/i-dont-exist.db"}
+  let(:bad_path) { "./not-existing/database.db"}
   let(:good_path) { "./caja.db" }
   
   it "should raise an error if the data store can not be created" do
-    expect { Caja::Box.new(bad_path) }.to raise_error(Caja::IOError)
-    PathName::File.exist?(bad_path).should_not be_true
+    expect { Caja::Box.new(bad_path) }.to raise_error(IOError)
+    Pathname::File.exist?(bad_path).should_not be_true
   end
 
-  it "should create a new file in case the initialization is successful" do
-    before { fail if PathName::File.exist?(good_path) }
-    after { PathName::File.delete(good_path) }
-
-    box = Caja::Box.new(good_path)
-    PathName::File.exist?(good_path).should be_true
+  context "initialization succesful" do
+    before { Pathname::File.delete(good_path) if Pathname::File.exist?(good_path) }
+    it "should create a new file after successful store operation" do
+      box = Caja::Box.new(good_path)
+      box.store("test key", "test value")
+      Pathname::File.exist?(good_path).should be_true
+    end
   end
   
 end
